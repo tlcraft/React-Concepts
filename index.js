443cf63442ba22c3c1a4336483d00ab484f36786
tlcraft <tlcraft@users.noreply.github.com>
2018-07-30 19:16:56 -0600

    Using JS with JSX for name formatting

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe'
+}
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
-  );
+);
