435b6ba85dd08c0acd2354d6422bd5b54fc729ab
tlcraft <tlcraft@users.noreply.github.com>
2018-08-07 22:47:59 -0600

    Add Login and Logout components

--- index.js
+++ index.js
@@ -102,6 +102,56 @@ class Toggle extends React.Component {
   }
 }
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        {button}
+      </div>
+    )
+  }
+}
+
 const user = {
   firstName: 'John',
   lastName: 'Doe'
@@ -115,6 +165,7 @@ function App() {
       <Clock />
       <Clock />
       <Toggle />
+      <LoginControl />
     </div>
   );
 }
