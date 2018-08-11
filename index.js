910b1743d25645e065fa447cb273647db5ff48a9
tlcraft <tlcraft@users.noreply.github.com>
2018-08-11 10:26:12 -0600

    Adding a Controlled Component

--- index.js
+++ index.js
@@ -170,7 +170,42 @@ class LoginControl extends React.Component {
   }
 }
 
-const user = {
+class NameForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event){
+    alert('A name was submitted: ' + this.state.value);
+    user.firstName = this.state.value;
+    event.preventDefault();
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="submit" />
+        <div>
+          {user.firstName}
+        </div>
+      </form>
+    );
+  }
+}
+
+let user = {
   firstName: 'John',
   lastName: 'Doe'
 }
@@ -205,6 +240,7 @@ function App() {
       <Toggle />
       <LoginControl />
       <NumberList numbers={numbers}/>
+      <NameForm />
     </div>
   );
 }
