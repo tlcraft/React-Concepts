d76621f9952647b0b2bc454a45c004000b771e39
tlcraft <tlcraft@users.noreply.github.com>
2018-08-16 19:33:53 -0600

    Adding a calculator class component

--- index.js
+++ index.js
@@ -354,6 +354,33 @@ function BoilingVerdict(props) {
   return <p>The water would not boil.</p>;
 }
 
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+
+        <BoilingVerdict 
+          celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
 
 
 function App() {
@@ -373,6 +400,7 @@ function App() {
       <ReservationForm />
       <BoilingVerdict celsius="102" />
       <BoilingVerdict celsius="98" />
+      <Calculator />
     </div>
   );
 }
